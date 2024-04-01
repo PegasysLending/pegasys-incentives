68d9e05bc38b2fb86814b1ae40f4dd302777f514
David Potolski Lafetá <davidpotolskilafeta@gmail.com>
2024-04-01 18:09:15 -0300

    updates incentives address

--- tasks/helpers/active_incentives.ts
+++ tasks/helpers/active_incentives.ts
@@ -9,35 +9,35 @@ task(`active-incentives`, `Make incentives active`)
     await BRE.run('set-DRE');
     
     // Impl address
-    const incentivesAddress = '0xc6AA3eD49053567b3eE7fcD4206324974ED0bD89';
-    const distributionSeconds = 7776000 // 90 days
+    const incentivesAddress = '0x41aA26266f9120FAFf40517e45d395141b2B2C0b';
+    const distributionSeconds = 86400 // 1 day
     // aTokenAddress and variableDebtTokenAddress is deployed by Lending-Contracts project, stored in deployed-contracts.json.
     // the total emissions[0] for aTokenAddress per seconds
     //           emissions[1] for variableDebtTokenAddress per seconds
     const tokenPairs = [
       {
         // WSYS: '0x65b28cBda2E2Ff082131549C1198DC9a50328186',
-        aTokenAddress: '0xed79570e50f4f46aa183c2367bD3C2017872DcC5',
-        variableDebtTokenAddress: '0x0ebf07118530bdc161cDb1b280F112480A87fAff',
-        emissions:[1706018518518520,1706018518518520],
+        aTokenAddress: '0x56Ef5b22fE07901E5807A85823bCF60c897Ff987',
+        variableDebtTokenAddress: '0x5E3BBB63431E365073FB1317c71B3402727Eac85',
+        emissions:[1,1],
       },
       {
         // WBTC: '0x386aFa4cED76F3Ddd5D086599030fC21B7Ad9c10',
-        aTokenAddress: '0x8b08cF476e50FfAa393AA60A52ea642235bf689F',
-        variableDebtTokenAddress: '0x84c4EC1D3cA47CC416f72321C397F836a5AF4350',
-        emissions: [92939814814815,92939814814815],
+        aTokenAddress: '0x27FA7f72013013CDDf0eC4C5b6f7c03FdD351FC0',
+        variableDebtTokenAddress: '0xccB999cc0b15aD2E730B0b3aB1bfbba7CF6526bF',
+        emissions: [1,1],
       },
       {
         // WETH: '0xFE0e902E5F363029870BDc871D27b0C9C46c8b80',
-        aTokenAddress: '0xaa7946882EA748D3D6BaB1d7443a69C4D2c95E56',
-        variableDebtTokenAddress: '0x8ee9241b4d9B069fABbbf5e2284FCdA16ca25CaD',
-        emissions: [5291203703703700,5291203703703700],
+        aTokenAddress: '0xeF28d8e9F1b6764a3F81D4e45f51F22e3122E7c2',
+        variableDebtTokenAddress: '0x91e2ED9dc1ad36EF1C4E2506bA900EF6c9D1816A',
+        emissions: [1,1],
       },
       {
         // USDT: '0xd270B0EdA02c6fEF5E213Bc99D4255B9eDd22617',
-        aTokenAddress: '0x41Fa181f8055e7407e8E3f6f28BC0CdDC134570D',
-        variableDebtTokenAddress: '0x6ff0b570804D0e1dfbaC08D2F13A94FD7a4Fc4dB',
-        emissions:[1706018518518520,1706018518518520],
+        aTokenAddress: '0xE64d5433f260641Dd75255a80e9e6bC6aADCBD7D',
+        variableDebtTokenAddress: '0x0B22245F9Ee420fE228A7B7D611c600a2E1FD62A',
+        emissions:[1,1],
       }
     ]
 
